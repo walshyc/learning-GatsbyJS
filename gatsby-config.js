dc4dc43c50157f4e4f95205eed18f8bf08e409e0
Conor Walsh <conorwalsh0703@gmail.com>
2020-04-26 16:02:12 +0100

    setup contentful as CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,13 @@ module.exports = {
     author: "Conor Walsh",
   },
   plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-sass`,
     {
       resolve: "gatsby-source-filesystem",
